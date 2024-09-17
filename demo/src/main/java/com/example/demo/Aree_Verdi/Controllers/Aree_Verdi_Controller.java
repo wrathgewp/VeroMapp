41aadcef02f4c123c22d14d20df690945f335b2c
martaroech <luca.martari@icloud.com>
2024-09-17 16:34:08 +0200

    1.2

--- demo/src/main/java/com/example/demo/Aree_Verdi/Controllers/Aree_Verdi_Controller.java
+++ demo/src/main/java/com/example/demo/Aree_Verdi/Controllers/Aree_Verdi_Controller.java
@@ -3,8 +3,19 @@ package com.example.demo.Aree_Verdi.Controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.Aree_Verdi.Models.Aree_Verdi;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 @RestController
 @RequestMapping("/aree_verdi")
 public class Aree_Verdi_Controller {
     
+    @GetMapping("get/all")
+    public List<Aree_Verdi> getAll() {
+        return null;
+    }
 }
--- demo/src/main/java/com/example/demo/Aree_Verdi/Services/Aree_Verdi_ServiceDB.java
+++ demo/src/main/java/com/example/demo/Aree_Verdi/Services/Aree_Verdi_ServiceDB.java
@@ -1,8 +1,22 @@
 package com.example.demo.Aree_Verdi.Services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.Aree_Verdi.Models.Aree_Verdi;
+import com.example.demo.Aree_Verdi.Repositories.Aree_Verdi_Repositories;
+
 
 @Service
 public class Aree_Verdi_ServiceDB {
-    // DA COMPILARE
+    @Autowired
+    private Aree_Verdi_Repositories aree_verdi_repositories;
+
+    @Transactional(readOnly = true)
+    public List<Aree_Verdi> findAll() {
+        return aree_verdi_repositories.findAll();
+    }
 }
